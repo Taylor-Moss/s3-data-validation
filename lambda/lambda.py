868db095f936b5ea1669ca60a82b1fd91c880905
Taylor Moss <tayloramoss1@gmail.com>
2025-12-13 14:29:34 -0600

    started lambda.py

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,1 +1,11 @@
-# placeholder
+import boto3
+import csv
+from datetime import datetime
+
+def lambda_handler(event, context):
+
+    # resource
+    s3 = boto3.resource('s3')
+
+    # get bucket name and the csv file name
+    
--- zip.py
+++ zip.py
@@ -2,34 +2,13 @@ import boto3, botocore, zipfile, os
 from pathlib import Path
 
 # variables
-region = 'us-east-1'
-bucket_name = 'lambda-bucket-0214'
+bucket_name = 'LambdaBucketName'
 lambda_file_path = Path(r"E:/Scripts/projects/s3-data-validation/lambda/lambda.py")
 zip_name = lambda_file_path.parent / 'lambda.zip'
 s3_key = f"lambda/{zip_name}"
 
 # client
-s3 = boto3.client('s3', region_name=region)
-
-# create bucket
-def create_bucket_with_versioning():
-    try:
-        s3.create_bucket(
-            Bucket=bucket_name,
-            CreateBucketConfiguration={"LocationConstraint": region}
-                         )
-    except botocore.exceptions.ClientError as e:
-        error_code = e.response['Error']['Code']
-        if error_code == 'BucketAlreadyOwnedByYou':
-            print(f"Bucket '{bucket_name}' already exists and is owned by you.")
-    
-        elif error_code == 'BucketAlreadyExists':
-            raise RuntimeError(
-                print(f"Bucket name '{bucket_name}' already exists and is owned by another AWS account.")
-            )
-
-        else:
-            raise
+s3 = boto3.client('s3')
 
 # zip the .py file
 def zip_lambda_file():
@@ -50,6 +29,5 @@ def upload_zip_to_s3():
     print(f"Uploaded {zip_name} to s3://{bucket_name}/{s3_key}")
 
 if __name__ == '__main__':
-    create_bucket_with_versioning()
     zip_lambda_file()
     upload_zip_to_s3()
